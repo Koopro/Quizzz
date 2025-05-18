51200606843c9edf7d0282b9754e9809ed81a5c6
koopro <christian.schallner@icloud.com>
2025-05-18 16:48:21 +0200

    Fix: Update styles and configuration for production

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,8 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/heartmaster' : ''
+			base: process.env.NODE_ENV === 'production' ? '/heartmaster' : '',
+			relative: false
 		}
 	}
 };
